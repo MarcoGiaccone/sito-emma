d1e310e46a4b3eddf5b53570b825f34f14404956
Marco Giaccone <marco.giaccone@edu.itspiemonte.it>
2025-11-16 00:58:21 +0100

    installato bootstrap,  creato componente form progetto nel manager

---
+++ frontend/src/app/components/reserved-project-form/reserved-project-form.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reserved-project-form',
+  imports: [],
+  templateUrl: './reserved-project-form.html',
+  styleUrl: './reserved-project-form.css'
+})
+export class ReservedProjectForm {
+
+}
